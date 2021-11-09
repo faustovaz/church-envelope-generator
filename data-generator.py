39caa056cd770c243a99198cddf85b7743b1ee8a
Jefferson Fausto Vaz <faustovaz@gmail.com>
2021-11-08 21:28:25 -0300

    Removed snipet file and changed some variable names.

--- data-generator.py
+++ data-generator.py
@@ -4,21 +4,27 @@ import time
 import sys
 import csv
 
+DATA_LEN = 300
+
 if __name__ == '__main__':
     fakes = []
     n = 0
-    while n <= 100:
-        response = requests.get("https://api.namefake.com/portuguese-brazil/female/")
+    while n <= DATA_LEN:
+        gender = "male" if  random.randint(0,1) == 0 else "female"
+        response = requests.get(
+                    f'https://api.namefake.com/portuguese-brazil/{gender}/')
         if response.ok:
             fake_data = response.json()
-            data = (random.randint(1000, 9999), fake_data['name'], fake_data['phone_w'])
+            data = (random.randint(1000, 9999),
+                    fake_data['name'],
+                    fake_data['phone_w'])
             fakes.append(data)
             time.sleep(3)
             print(data)
         else:
-            print("--------- error ---------")
+            print("Oops, something wrong with namefake api.")
             sys.exit()
         n += 1
-    with open('dizimistas.csv', 'w', encoding='utf-8') as csvfile:
+    with open('fake_data.csv', 'w', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_ALL)
         writer.writerows(fakes)
